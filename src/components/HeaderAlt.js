95be5c562e8c83182f3723667e1c4a9fd4fc46f2
Bryan C Guner <bryan.guner@gmail.com>
2021-11-09 20:23:13 +0000

    -update

--- src/components/HeaderAlt.js
+++ src/components/HeaderAlt.js
@@ -7,7 +7,9 @@ export default class HeaderAlt extends React.Component {
         return (
             <header className="site-header-alt">
                 <nav className="site-navigation-alt" aria-label="Main Navigation">
-                    <Link className="home-link" href={withPrefix('/')}><span className="icon-arrow-left" aria-hidden="true" /> All Articles</Link>
+                    <Link className="home-link" href={withPrefix('/')}>
+                        <span className="icon-arrow-left" aria-hidden="true" /> All Articles
+                    </Link>
                 </nav>
             </header>
         );
